5a6bdc20fdf3e1a6075d5c813a26c43ecc25c2a5
Jelly Bean <jelly-b@sina.com>
2020-06-03 23:34:22 +0800

    Initial version of sand demo project.

---
+++ com.firstlinecode.sand.demo/app/src/main/java/com/firstlinecode/sand/demo/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.firstlinecode.sand.demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+	public void register(View view) {
+		startActivity(new Intent(this, RegisterActivity.class));
+	}
+}
